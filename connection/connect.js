ce64070302e00692f30ed8eec0f3a43db6279d1d
Unknown <rynemgar@gmail.com>
2018-06-27 18:18:50 +0100

    Initial commit

---
+++ connection/connect.js
@@ -0,0 +1,14 @@
+var sql = require("mssql");
+var connect = function()
+{
+var conn = new sql.ConnectionPool({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASS,
+        port: process.env.MYSQL_PORT,
+        database: process.env.MYSQL_DB,
+});
+    return conn;
+};
+
+module.exports = connect;
