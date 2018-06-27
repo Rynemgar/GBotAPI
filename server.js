9fdc84c4d7805d37eb5d685a0f37dfad286452ac
Unknown <rynemgar@gmail.com>
2018-06-27 18:19:02 +0100

    API so far

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var port = process.env.port || 1337;
+
+var levelsController = require('./Controller/Level')();
+
+app.use("/api/levels", levelsController);
+
+app.listen(port, function() {
+    var datetime = new Date();
+    var message = "Server running on Port:- " + port + " Started at:- " + datetime;
+    console.log(message);
+});
